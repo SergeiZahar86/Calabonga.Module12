ab923df8d8e9e0147650b5730b8ba022e9621a33
Сергей Захаров <sergei86zahar@mail.ru>
2021-08-15 09:07:06 +0700

    регистрация получателя

--- Calabonga.Module12/Calabonga.Module12.Web/AppStart/ConfigureServices/ConfigureServicesMassTransit.cs
+++ Calabonga.Module12/Calabonga.Module12.Web/AppStart/ConfigureServices/ConfigureServicesMassTransit.cs
@@ -1,4 +1,6 @@
-﻿using Calabonga.Microservices.Core.Exceptions;
+﻿using Calabonga.Contracts;
+using Calabonga.Microservices.Core.Exceptions;
+using Calabonga.Module12.Web.MassTransit;
 using MassTransit;
 using MassTransit.Definition;
 using Microsoft.Extensions.Configuration;
@@ -41,6 +43,10 @@ namespace Calabonga.Module12.Web.AppStart.ConfigureServices
                     cfg.UseRawJsonSerializer();
                     cfg.UseHealthCheck(context);
                     cfg.ConfigureEndpoints(context, SnakeCaseEndpointNameFormatter.Instance);
+                    cfg.ReceiveEndpoint(Constants.NotificationQueueNameWarehouse, e =>
+                    {
+                        e.Consumer<ApplicationUserCreatedConsumer>();
+                    });
                 });
             });
 
---
+++ Calabonga.Module12/Calabonga.Module12.Web/MassTransit/ApplicationUserCreatedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using Calabonga.Contracts;
+using MassTransit;
+using System.Threading.Tasks;
+
+namespace Calabonga.Module12.Web.MassTransit
+{
+    public class ApplicationUserCreatedConsumer : IConsumer<IApplicationUserCreated>
+    {
+        public Task Consume(ConsumeContext<IApplicationUserCreated> context)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
